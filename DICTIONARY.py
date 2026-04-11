ab7b085060f1020e0a5788ff56af2c5d0aae4d5f
Akshhitt? <akshittdesaiwal08@gmail.com>
2026-04-11 20:16:13 +0530

    Add files via upload
    tried dictionary todayy

---
+++ DICTIONARY.py
@@ -0,0 +1,23 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:57:36) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> valopoints={ 'round 1' : 123 , 'round 2' 456 , 'round 3' :789 , 'round 5' :012 }
+SyntaxError: invalid syntax
+>>> 
+>>> 
+
+
+
+valopoints={ 'round 1' : 123 , 'round 2' 456 , 'round 3' :789 , 'round 5' :012 }
+SyntaxError: invalid syntax
+>>> sample={3+2j: "complex",5j:"complex",19+2j: "complex",18.1:"float"}
+>>> sample
+{(3+2j): 'complex', 5j: 'complex', (19+2j): 'complex', 18.1: 'float'}
+>>> valopoints={ 'round 1' : 123 , 'round 2' 456 , 'round 3' :789 , 'round 5' :012 }
+SyntaxError: invalid syntax
+>>> >>> valopoints={ 'round 1' : 123 , 'round 2': 456 , 'round 3' :789 , 'round 5' :012 }
+SyntaxError: invalid syntax
+>>> valopoints={ 'round 1' : 123 , 'round 2': 456 , 'round 3' :789 , 'round 5' :012 }
+SyntaxError: invalid token
+>>> valopoints={ 'round 1' : 123 , 'round 2': 456 , 'round 3' : 789 , 'round 5' : 012 }
+SyntaxError: invalid token
+>>> 
---
+++ day2.md
@@ -0,0 +1,15 @@
+# DICTIONARY
+-dIictionary is not an orderd collection
+-key is immutable
+number 
+string
+tuple 
+but not list
+key cant appear again and again
+no index numbers
+get(used)
+len= length
+max = largest or lastin alphabetical order 
+min= opposite of max
+del for delete
+clear to delete all
